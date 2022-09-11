8bd98e50a3d0ee690dfa10e3ed05ac3ec0250b90
Mukesh Patel <110808838+pmukesh31@users.noreply.github.com>
2022-09-11 16:02:30 +0530

    Create Package.java

---
+++ Access-modifer/Package.java
@@ -0,0 +1,9 @@
+package myPackage;
+import java.Encapsulation.MultipleConst;
+public class Package{
+    public static void main(String args[])
+    {
+        Circle c2=new Circle(5);
+        System.out.println("Hello This is Demo package");
+    }
+}
