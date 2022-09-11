989b6e6d9da6ada44e3d2a2a97fe11d03dba40c3
Mukesh Patel <110808838+pmukesh31@users.noreply.github.com>
2022-09-11 16:04:07 +0530

    Create firstInterface.java

---
+++ Interfaces/MyInterface/firstInterface.java
@@ -0,0 +1,7 @@
+package Myinterface;
+public interface firstInterface{
+    //int x; We need to assign value to it;
+    int x=100;//auto matic it is final static int x=100;
+    void run();//by default it is public abstract
+    //public abstract final int run();//gives error as method cannot be final in interface
+}
